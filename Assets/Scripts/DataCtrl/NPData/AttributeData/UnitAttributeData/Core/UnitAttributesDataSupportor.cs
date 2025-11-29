38693d780eb2c6dc149911b6a223ac71a0323850
MiniGiant <soulmasterzxf@proton.me>
2025-11-29 20:02:40 +0800

    Add description to UnitAttributesDataSupportor

--- Assets/Scripts/DataCtrl/NPData/AttributeData/UnitAttributeData/Core/UnitAttributesDataSupportor.cs
+++ Assets/Scripts/DataCtrl/NPData/AttributeData/UnitAttributeData/Core/UnitAttributesDataSupportor.cs
@@ -1,15 +1,12 @@
-//------------------------------------------------------------
-// Author: YanYuMiLiBanShiShang
-// Mail: 1778139321@qq.com
-// Date: 2019-07-25 19:20:42
-//------------------------------------------------------------
-
 using System.Collections.Generic;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization.Options;
 
 namespace Ebonor.DataCtrl
 {
+    /// <summary>
+    /// Container for unit attribute nodes keyed by unit id; used during graph export/import.
+    /// </summary>
     public class UnitAttributesDataSupportor
     {
         [BsonDictionaryOptions(DictionaryRepresentation.ArrayOfArrays)]

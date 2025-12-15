d6a87bc446a56a7b7714cbdea74b6a316a2f6472
MiniGiant <soulmasterzxf@proton.me>
2025-12-15 10:42:42 +0800

    Update scene loader and prefab

--- Assets/Scripts/Manager/SceneLoaderService.cs
+++ Assets/Scripts/Manager/SceneLoaderService.cs
@@ -1,12 +1,9 @@
-using System;
 using Cysharp.Threading.Tasks;
 using Ebonor.DataCtrl;
 using Ebonor.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Zenject;
-using Ebonor.UI;
-using UnityEngine.Assertions; // Assuming UIScene_Loading is here or we use IUIService to get it
 using UObject = UnityEngine.Object;
 namespace Ebonor.Manager
 {
@@ -15,16 +12,15 @@ namespace Ebonor.Manager
         
         private static readonly ILog log = LogManager.GetLogger(typeof(SceneLoaderService));
         
-        private readonly ZenjectSceneLoader _zenjectSceneLoader;
         private readonly ResourceLoader _resourceLoader;
-        private readonly DiContainer _container;
+        private readonly IInstantiator _instantiator;
         
         // ZenjectSceneLoader is a built-in Zenject service for loading scenes with bindings.
         // If we don't need extra bindings, standard SceneManager works too, but ZenjectSceneLoader is safer for Contexts.
-        public SceneLoaderService(ResourceLoader resourceLoader, DiContainer container)
+        public SceneLoaderService(ResourceLoader resourceLoader, IInstantiator container)
         {
             _resourceLoader = resourceLoader;
-            _container = container;
+            _instantiator = container;
         }
 
         public async UniTask LoadSceneAsync(string sceneName)
@@ -42,8 +38,8 @@ namespace Ebonor.Manager
             // Use Zenject's container to instantiate the prefab. 
             // This ensures the GameObjectContext is properly initialized and parented if needed.
             var prefab = obj as GameObject;
-            var instance = _container.InstantiatePrefab(prefab);
-            
+            var instance = _instantiator.InstantiatePrefab(prefab);
+            instance.name = prefab.name;
             log.Debug($"[SceneLoaderService] Instantiated: {instance.name}, Active: {instance.activeInHierarchy}");
             
             // Ensure root before moving to current scene (required by MoveGameObjectToScene)
